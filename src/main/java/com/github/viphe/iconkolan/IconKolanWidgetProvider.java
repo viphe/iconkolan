70682fcfcdd68e8a08000f4de4633edd8e7e8dc1
Philippe Vosges <philippe.vosges@gmail.com>
2014-05-22 00:57:30 -0700

    basic display of app icon and name; launch app on click

--- src/main/java/com/github/viphe/iconkolan/IconKolanWidgetProvider.java
+++ src/main/java/com/github/viphe/iconkolan/IconKolanWidgetProvider.java
@@ -1,9 +1,17 @@
 package com.github.viphe.iconkolan;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.widget.RemoteViews;
 
 public class IconKolanWidgetProvider extends AppWidgetProvider {
@@ -15,15 +23,40 @@ public class IconKolanWidgetProvider extends AppWidgetProvider {
         for (int i=0; i<N; i++) {
             int appWidgetId = appWidgetIds[i];
 
-            // Create an Intent to launch ExampleActivity
-//            Intent intent = new Intent(context, ExampleActivity.class);
-//            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            PackageManager packageManager = context.getPackageManager();
+            ApplicationInfo applicationInfo;
+            try {
+                applicationInfo = packageManager.getApplicationInfo(
+                        "com.enterpriseappzone", PackageManager.GET_UNINSTALLED_PACKAGES);
+            } catch (PackageManager.NameNotFoundException e) {
+                continue;
+            }
 
-//            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-//            views.setOnClickPendingIntent(R.id.button, pendingIntent);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+            views.setTextViewText(R.id.app_name, applicationInfo.loadLabel(packageManager));
+            views.setImageViewBitmap(R.id.app_icon, drawableToBitmap(applicationInfo.loadIcon(packageManager)));
+
+            Intent intent = packageManager.getLaunchIntentForPackage("com.enterpriseappzone");
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.app_launcher, pendingIntent);
 
             // Tell the AppWidgetManager to perform an update on the current app widget
-//            appWidgetManager.updateAppWidget(appWidgetId, views);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
+
+
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+
 }
